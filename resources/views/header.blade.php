76922c686f742bc4bdac7d4872741e5f8e006ac5
Salma Novita <salmanovita@student.ub.ac.id>
2023-10-23 06:57:45 +0700

    header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="/dist/output.css" rel="stylesheet">
 </head>
 <body>
-    <h1>HALO</h1>
+    <h1>HELO</h1>
 </body>
 </html>
