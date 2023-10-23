9fb0b9fb6efe4dbfe6d146836d2424fd6083a838
fikriyarachmaa <fikriyarachma02@student.ub.ac.id>
2023-10-23 22:47:12 +0700

    batik shop update

--- resources/views/batikShop.blade.php
+++ resources/views/batikShop.blade.php
@@ -5,38 +5,88 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-cream">
+    @include('components.header')
 <div class="relative bg-green-pastel font-Raleway">
         <div class="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7">
             <div class="flex items-center flex-wrap px-2 md:px-0">
                 <div class="relative lg:w-6/12 lg:py-24 xl:py-32">
-                    <h1 class="font-bold text-4xl text-yellow-900 md:text-5xl lg:w-10/12">Soul Of Indonesia</h1>
+                    <h1 class="font-bold text-4xl text-green-dark md:text-5xl lg:w-10/12">Selamat Datang di Batik Shop !</h1>
+                    <p class="text-base text-green-dark md:text-2xl lg:w-10/12 py-4">Eksplorasi produk seleramu lalu segera hubungi seller untuk miliki barang impianmu!</p>
+                    <!--search bar -->
                     <form action="" class="w-full mt-12">
-                        <div class="relative flex p-1 rounded-full bg-white border border-yellow-200 shadow-md md:p-2">
-                            <select class="hidden p-3 rounded-full bg-transparent md:block md:p-4" name="domain" id="domain">
-                                <option value="design">FastFood</option>
-                                <option value="development">Restaurant</option>
-                                <option value="marketing">Marketing</option>
-                            </select>
-                            <input placeholder="Your favorite food" class="w-full p-4 rounded-full" type="text">
-                            <button type="button" title="Start buying" class="ml-auto py-3 px-6 rounded-full text-center transition bg-gradient-to-b from-yellow-200 to-yellow-300 hover:to-red-300 active:from-yellow-400 focus:from-red-400 md:px-12">
-                                <span class="hidden text-yellow-900 font-semibold md:block">
-Search
-                                </span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 mx-auto text-yellow-900 md:hidden" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                </svg>
-                            </button>
+                        <div class="relative flex p-1 rounded-full bg-white shadow-md md:p-2">
+                            <input placeholder="Search by category" class="w-full p-4 rounded-full" type="text">
+                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 
+                                dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                         </div>
                     </form>
-                    <p class="mt-8 text-gray-700 lg:w-10/12">Sit amet consectetur adipisicing elit. <a href="#" class="text-yellow-700">connection</a> tenetur nihil quaerat suscipit, sunt dignissimos.</p>
                 </div>
+                <!--hero image -->
                 <div class="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
-                    <img src="https://tailus.io/sources/blocks/food-delivery/preview/images/food.webp" class="relative" alt="food illustration" loading="lazy" width="4500" height="4500">
+                    <img src="/img/hero-batik.png" class="relative" alt="illustration" loading="lazy" width="4500" height="4500">
                 </div>
             </div>
         </div>
     </div>
+    <!--hero image -->
+    <br> <br>
+    <!--kategori produk -->
+    <section class="my-10 font-Raleway">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                <h2 class="mb-4 text-xl font-semibold text-gray-900 dark:text-white">Kategori Produk</h2>
+                <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          <category class="max-w-xs">
+              <a href="#">
+                  <img src="/img/kain.png" class="mb-5 rounded-lg" alt="Image 1">
+              </a>
+          </category>
+          <category class="max-w-xs">
+              <a href="#">
+              <img src="/img/pakaian.png" class="mb-5 rounded-lg" alt="Image 2">
+              </a>
+          </category>
+          <category class="max-w-xs">
+              <a href="#">
+                  <img src="/img/aksesoris.png" class="mb-5 rounded-lg" alt="Image 3">
+              </a>
+          </category>
+          <category class="max-w-xs">
+              <a href="#">
+                  <img src="/img/kerajinan.png" class="mb-5 rounded-lg" alt="Image 4">
+              </a>
+          </category>
+      </div>                
+        </div>
+    </section>
+  <!--kategori produk -->
   
+  <!-- produk -->
+  <section class="my-10 font-Raleway">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <h2 class="mb-4 text-xl font-semibold text-gray-900 dark:text-white">Kategori Produk</h2>
+            <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                
+            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="p-8 rounded-t-lg" src="/img/batiksolo.jpg" alt="product image" />
+    </a>
+    <div class="px-5 pb-5">
+        <a href="#">
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Kain Batik Solo</h5>
+        </a>
+        <p>Kota Pengiriman : Solo</p>
+        <div class="flex items-center justify-between">
+            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 110.000</span>
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div>
+    </div>
+</div>
+
+        
+        </div>
+  </section>
+  <!-- produk -->
+  @include('components.footer')
 </body>
 </html>
