4a960da9a22d1fa74b82e4cacd6129b94bc50b3b
lintangea <lintangeka@student.ub.ac.id>
2023-11-04 16:42:12 +0700

    menambah landing page dan regis

--- resources/views/auth/regis.blade.php
+++ resources/views/auth/regis.blade.php
@@ -5,52 +5,51 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-gradient-to-tl from-green-pastel to-green-light" >
 @include('components.header')
-<section class="bg-cream">  
-<div class="h-screen bg-indigo-100 flex justify-center items-center">
-	<div class="lg:w-2/5 md:w-1/2 w-2/3 pt-20">
-		<form class="bg-white p-10 rounded-lg shadow-lg min-w-full ">
-			<h1 class="text-green-dark font-bold text-2xl mb-1 text-center">Mari Bergabung Menjadi Penjual</h1>
-			<div>
-				<label class="text-gray-800 font-semibold block my-3 text-md" for="nama">Nama Pemilik Usaha :</label>
-				<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Tulis Disini..." />
-      </div>
-				<div>
-					<label class="text-gray-800 font-semibold block my-3 text-md" for="usaha">Nama Usaha :</label>
-					<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="usaha" id="usaha" placeholder="Tulis Disini..." />
-      </div>
-	            <div>
-					<label class="text-gray-800 font-semibold block my-3 text-md" for="kontak">Nomor Kontak :</label>
-					<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="number" name="kontak" id="kontak" placeholder="Tulis Disini..." />
-      </div>
-	            <div>
-					<label class="text-gray-800 font-semibold block my-3 text-md" for="alamat">Alamat Bisnis :</label>
-					<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="alamat" id="alamat" placeholder="Tulis Disini..." />
-      </div>
-	            <div>
-					<label class="text-gray-800 font-semibold block my-3 text-md" for="email">Alamat Email :</label>
-					<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="Tulis Disini..." />
-      </div>
-					<div>
-						<label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password :</label>
-						<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="Konfirmasi Kata Sandi" />
-      </div>
-						<div>
-							<label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password :</label>
-							<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="confirm password" />
-      </div>
-	  <div>
-                         <label class="text-gray-800 font-semibold block my-3 text-md" for="logo_bisnis">Logo Bisnis :</label>
-                         <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="file" name="logo_bisnis" id="logo_bisnis" accept="image/*" />
-      </div>
-
-							<button type="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Register</button>
-							<button type="submit" class="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Login</button>
-		</form>
-	</div>
+<div style="display: flex; justify-content: center; align-items: center; margin-top: 20vh; margin-bottom: 8 vh;">
+    <img src="\img\logoSOI.png" alt="" width="200" height="200" >
 </div>
-</section>
 
+<div class="flex justify-center items-center mt-4 ">
+<form class="bg-white p-10 my-12 rounded-lg shadow-lg w-" style="position: relative;">
+
+<h1 class="text-green-900 font-bold text-2xl mb-1 text-center">Mari Bergabung Menjadi Penjual</h1>
+
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="nama">Nama Pemilik Usaha :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="text" name="username" id="username" placeholder="Tulis Disini..." />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="usaha">Nama Usaha :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="text" name="usaha" id="usaha" placeholder="Tulis Disini..." />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="kontak">Nomor Kontak :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="number" name="kontak" id="kontak" placeholder="Tulis Disini..." />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="alamat">Alamat Bisnis :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="text" name="alamat" id="alamat" placeholder="Tulis Disini..." />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Alamat Email :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="text" name="email" id="email" placeholder="Tulis Disini..." />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="text" name="password" id="password" placeholder="Konfirmasi Kata Sandi" />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="text" name="confirm" id="confirm" placeholder="confirm password" />
+    </div>
+    <div>
+        <label class="text-gray-800 font-semibold block my-3 text-md" for="logo_bisnis">Logo Bisnis :</label>
+        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg" style="outline: 1px solid green;" type="file" name="logo_bisnis" id="logo_bisnis" accept="image/*" />
+    </div>
+    <button type="submit" class="w-full h-50 mt-6 bg-green-500 rounded-lg px-1 py-2 text-lg text-white tracking-wide font-bold font-sans hover:bg-green-300 hover:text-white mx-auto block" >Daftar</button>
+</form>
+</div>
 </body>
 </html>
--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css') 
 </head>
-<body id="landingpage">
+<body id="landingpage" class="bg-cream">
 @include('components.header')
-<section class="bg-cream" >
+<section>
     <!-- Ini adalah halaman awal -->
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:grid-cols-12">
         <div class="lg:col-span-7 flex flex-col justify-center">
@@ -26,10 +26,10 @@
 </section>
 
 
-<section class="bg-cream">
+<section>
      <!-- Ini adalah halaman kedua -->
      <div class="min-w-screen min-h-screen flex items-center justify-center py-12  ">
-    <div class="w-full flex rounded-lg bg-gradient-to-r   from-green-light via-30% to-green-pastel to-90% shadow-lg " style="margin: 50px; padding: 0px; position: relative; pt-12">
+    <div class="w-full flex rounded-lg bg-gradient-to-r from-green-light via-30% to-green-pastel to-90% shadow-lg " style="margin: 50px; padding: 0px; position: relative; pt-12">
         <div class="w-1/2 p-5"> 
             <p class="text-lg md:text-xl lg:text-2xl text-green-darkest font-bold text-justify px-5 pt-12">
                 "Seni tradisional pembuatan kain yang unik dan khas dari Indonesia. 
@@ -39,13 +39,9 @@
             <div class="flex justify-center items-center pt-8">
             <button type="submit" class="bg-cream mt-5 py-3 px-6  rounded-2xl hover:bg-yellow-700 hover:-translate-y-1 transition-all duration-500 text-green-dark font-semibold mb-2">Galeri Batik</button>
             </div>
-
-
         </div>
-        
         <!-- Garis Vertikal -->
         <div class="w-1 border-l border-green-darkest"></div>
-        
         <div class="w-1/2 p-5 relative" style="display: flex; flex-direction: column; justify-content: flex-end; align-items: flex-end;">
             <h1 class="max-w-2xl mb-4 text-4xl font-bold leading-none md:text-5xl xl:text-6xl text-green-darkest text-center pt-12">Apa itu Batik Indonesia?</h1>
             <div class="position: relative; margin: 0; padding: 0;">
@@ -55,6 +51,69 @@
     </div>
 </div>
 </section>
+
+<section>
+<div class="flex flex-row space-x-4" >
+    <div class="relative flex w-96 flex-col rounded-xl bg-white text-gray-700 shadow-md ml-10">
+      <div class="relative mx-4 my-4 mt-4 h-96 overflow-hidden rounded-xl bg-white text-gray-700">
+        <img
+          src="img/batiksoganparang.png"
+          class="h-full w-full object-cover"
+        />
+        <div class="absolute inset-0 flex items-center justify-center text-black text-lg font-semibold opacity-0 hover:opacity-100 transition-opacity bg-white hover:bg-opacity-50 duration-300">
+        
+        Kain Batik Parang
+        <br>Rp 170.000</br>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative flex w-96 flex-col rounded-xl bg-white text-gray-700 shadow-md ml-20">
+      <div class="relative mx-4 my-4 mt-4 h-96 overflow-hidden rounded-xl bg-white text-gray-700">
+        <img
+          src="img/kemejabatikpria.png"
+          class="h-full w-full object-cover"
+        />
+        <div class="absolute inset-0 flex items-center justify-center text-black text-lg font-semibold opacity-0 hover:opacity-100 bg-white transition-opacity hover:bg-opacity-50 duration-300">
+        Kemeja Batik Pria
+          <br>Rp 130.000</br>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative flex w-96 flex-col rounded-xl bg-white text-gray-700 shadow-md ml-20">
+  <div class="relative mx-4 my-4 mt-4 h-96 overflow-hidden rounded-xl bg-white text-gray-700">
+    <img
+      src="img/rokpendek.png"
+      class="h-full w-full object-cover"
+    />
+    <div class="absolute inset-0 flex items-center justify-center text-black text-lg font-semibold opacity-0 hover:opacity-100 bg-white hover:bg-opacity-50 transition-opacity duration-300">
+      Rok Batik Remaja
+      <br>Rp 200.000</br>
+    </div>
+  </div>
+</div>
+
+    <div class="relative flex w-96 flex-col rounded-xl bg-white text-gray-700 shadow-md ml-20 mr-1">
+      <div class="relative mx-4 my-4 mt-4 h-96 overflow-hidden rounded-xl bg-white text-gray-700">
+        <img
+          src= "img/blousesandra.png"
+          class="h-full w-full object-cover"
+        />
+        <div class="absolute inset-0 flex items-center justify-center text-black text-lg font-semibold opacity-0 hover:opacity-100 hover:bg-opacity-50 transition-opacity bg-white duration-300">
+          Blouse Batik Wanita
+        <br>Rp 200.000</br>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+  
 
 </body>
 </html>
