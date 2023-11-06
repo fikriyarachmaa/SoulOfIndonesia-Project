d244606c1794727bcd4a0b3a3f9c02badb56feb2
Salma Novita <salmanovita@student.ub.ac.id>
2023-11-06 20:04:39 +0700

    revisi pengrajin produk

--- resources/views/pengrajin/produk.blade.php
+++ resources/views/pengrajin/produk.blade.php
@@ -152,7 +152,7 @@
             </table>
         </div>
                 <div class="flex justify-end pr-10">
-                    <a href="#" class="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <a href="#" class="flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-yellow border-gray-300 rounded-lg hover:bg-green-dark hover:text-cream">
                         Next
                         <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
