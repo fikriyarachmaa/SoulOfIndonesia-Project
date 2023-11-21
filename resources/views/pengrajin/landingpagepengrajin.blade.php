35cb51e00b5b7a0ab5f57071fedf282e43e73832
Salma Novita <salmanovita@student.ub.ac.id>
2023-11-21 11:00:16 +0700

    landingpage revise

--- resources/views/pengrajin/landingpagepengrajin.blade.php
+++ resources/views/pengrajin/landingpagepengrajin.blade.php
@@ -198,14 +198,7 @@
 
     <div class="grid gap-x-6 md:grid-cols-3 lg:gap-x-12">
       <div class="mb-12 md:mb-0">
-        <div class="mb-6 flex justify-center">
-          <img src="img/maria.jpg"
-            class="w-32 rounded-full shadow-lg dark:shadow-black/20" />
-        </div>
         <h5 class="mb-2 text-lg font-bold">Maria Smantha</h5>
-        <h6 class="mb-4 font-medium text-primary dark:text-primary-400">
-          Content Writer
-        </h6>
         <p class="mb-4">
         Website ini sangat bermanfaat untuk mengenal berbagai jenis batik di Indonesia. Melalui website ini bisa membantu saya sebagai pelanggan untuk membeli produk batik. 
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
@@ -213,48 +206,9 @@
               d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
           </svg>
         </p>
-        <ul class="mb-0 flex justify-center">
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m480 757 157 95-42-178 138-120-182-16-71-168v387ZM233 976l65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-        </ul>
       </div>
       <div class="mb-12 md:mb-0">
-        <div class="mb-6 flex justify-center">
-          <img src="img/lisa.jpg"
-            class="w-32 rounded-full shadow-lg dark:shadow-black/20" />
-        </div>
         <h5 class="mb-2 text-lg font-bold">Lisa Barito</h5>
-        <h6 class="mb-4 font-medium text-primary dark:text-primary-400">
-          Graphic Designer
-        </h6>
         <p class="mb-4">
           Melalui website ini saya dapat membeli produk batik, sehingga memudahkan saya membeli fashion yang mengangkat kebudayaan Indonesia. Serta saya senang bisa memakai karya batik dari para pengrajin batik.
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
@@ -262,48 +216,9 @@
               d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
           </svg>
         </p>
-        <ul class="mb-0 flex justify-center">
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-        </ul>
       </div>
       <div class="mb-0">
-        <div class="mb-6 flex justify-center">
-          <img src="img/baskara.jpg"
-            class="w-32 rounded-full shadow-lg dark:shadow-black/20" />
-        </div>
         <h5 class="mb-2 text-lg font-bold">Bagus Adi Kara</h5>
-        <h6 class="mb-4 font-medium text-primary dark:text-primary-400">
-          Marketing Specialist
-        </h6>
         <p class="mb-4">
          Saya senang dapat dengan mudah menemukan rekomendasi tempat wisata batik. Sehingga saya bisa mengajak anak saya liburan sekaligus mengenalkan warisan budaya Indonesia.
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
@@ -311,38 +226,6 @@
               d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
           </svg>
         </p>
-        <ul class="mb-0 flex justify-center">
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
-            </svg>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
-              <path fill="currentColor"
-                d="m323 851 157-94 157 95-42-178 138-120-182-16-71-168-71 167-182 16 138 120-42 178Zm-90 125 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-355Z" />
-            </svg>
-          </li>
-        </ul>
       </div>
     </div>
   </section>
